c49a408c245feba4711d729ae8d81783fef3e388
el-ethan <elethan@gmail.com>
2019-04-02 11:16:45 -0500

    Add non-working pencil icon

--- client/src/Incident.js
+++ client/src/Incident.js
@@ -6,12 +6,15 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
+import CreateIcon from '@material-ui/icons/Create';
+
 
 
 function Incident(props) {
     return (
         <Card className="incident-card">
             <DeleteRoundedIcon className="trash-can" onClick={() => props.onDelete(props._id)}/>
+            <CreateIcon className="pencil" />
             <CardContent>
                 <div className="card-section">
     	            <Typography variant="h5" component="h2">Problem:</Typography>
