ba77b5b8fd119cec754e6c6258bd5b8c886fd34c
el-ethan <elethan@gmail.com>
2019-07-21 06:15:01 -0500

    Refactor IncidentDialog

--- client/src/IncidentDialog.js
+++ client/src/IncidentDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -11,35 +11,30 @@ import Fab from '@material-ui/core/Fab';
 import {ContextSelector} from './ContextSelector';
 
 
-export default class IncidentDialog extends React.Component {
+function IncidentDialog(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-            problem: '',
-            solution: '',
-            selectedContext: props.appContext
-        };
+    const [modalOpen, setModelOpen] = useState(false)
+    const [problem, setProblem] = useState('');
+    const [solution, setSolution] = useState('');
+    const [selectedContext, setSelectedContext] = useState(props.appContext);
+
+    const handleContextSelection = (contextSelection) => {
+        setSelectedContext(contextSelection)
     }
 
-    handleContextSelection = (contextSelection) => {
-        this.setState({selectedContext: contextSelection});
-    }
-
-    handleClickOpen = () => {
-        this.setState({ open: true });
+    const handleClickOpen = () => {
+        setModelOpen(true);
     };
 
-    handleClose = () => {
-        this.setState({ open: false });
+    const handleClose = () => {
+        setModelOpen(false);
     };
 
-    postIncidentToDatabase = () => {
+    const postIncidentToDatabase = () => {
         const incidentObj = {
-            problem: this.state.problem,
-            solution: this.state.solution,
-            context: this.state.selectedContext
+            problem: problem,
+            solution: solution,
+            context: selectedContext
         };
 
         fetch('/api/incidents', {
@@ -50,62 +45,62 @@ export default class IncidentDialog extends React.Component {
         return incidentObj;
     }
 
-    save = () => {
-        const incident = this.postIncidentToDatabase();
-        this.props.onAddFunc(incident);
-        this.handleClose();
+    const save = () => {
+        const incident = postIncidentToDatabase();
+        props.onAddFunc(incident);
+        handleClose();
     }
 
-    render() {
-        return (
-            <div className="add-button" title="report incident">
-                <Fab color="primary" onClick={this.handleClickOpen}>
-                    <AddIcon />
-                </Fab>
-                <Dialog
-                    open={this.state.open}
-                    onClose={this.handleClose}
-                    aria-labelledby="form-dialog-title"
-                >
-                    <DialogTitle id="form-dialog-title">Report Incident</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Add details about the incident.
-                        </DialogContentText>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Problem"
-                            fullWidth
-                            multiline
-                            rowsMax="4"
-                            onChange={
-                            (event) => this.setState({problem: event.target.value})
-                            }
-                        />
-                        <TextField
-                            margin="dense"
-                            id="name"
-                            label="Solution"
-                            fullWidth
-                            onChange={
-                            (event) => this.setState({solution: event.target.value})
-                            }
-                        />
-                <ContextSelector setContext={this.handleContextSelection}
-                                 selectedContext={this.props.appContext} />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleClose} color="primary">
-                            Cancel
-                        </Button>
-                        <Button onClick={this.save} color="primary">
-                            Save
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </div>
-        );
-    }
+    return (
+        <div className="add-button" title="report incident">
+            <Fab color="primary" onClick={handleClickOpen}>
+                <AddIcon />
+            </Fab>
+            <Dialog
+                open={modalOpen}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Report Incident</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Add details about the incident.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Problem"
+                        fullWidth
+                        multiline
+                        rowsMax="4"
+                        onChange={
+                        (event) => setProblem(event.target.value)
+                        }
+                    />
+                    <TextField
+                        margin="dense"
+                        id="name"
+                        label="Solution"
+                        fullWidth
+                        onChange={
+                        (event) => setSolution(event.target.value)
+                        }
+                    />
+                    <ContextSelector setContext={handleContextSelection}
+                                     selectedContext={props.appContext} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={save} color="primary">
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
+
+export default IncidentDialog;
